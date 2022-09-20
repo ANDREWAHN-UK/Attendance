e9d8f7c2184feb11b937caf5613527f5c3fa5052
ANDREWAHNUK <ANDREW.AHN@HOTMAIL.CO.UK>
2022-09-20 12:21:33 +0100

    Connected to remotesql database

--- db/conn.php
+++ db/conn.php
@@ -1,10 +1,20 @@
  <?php
-    $host = 'localhost';
-    $db = 'attendance_db';
-    $user = 'root';
-    $pass = '';
-    $charset = 'utf8mb4';
 
+// this is development/local connection stuff
+    // $host = 'localhost';
+    // $db = 'attendance_db';
+    // $user = 'root';
+    // $pass = '';
+    // $charset = 'utf8mb4';  
+
+    // Below for remote database stuff, i.e. connecting to a non local database
+
+    $host = 'remotemysql.com';
+    $db = '1zFOJFQPiN';
+    $user = '1zFOJFQPiN';
+    $pass = '4MKeLecHpr';
+    $charset = 'utf8mb4';
+    
     $dsn = "mysql:host=$host; dbname=$db;charset=$charset";
 
     try
